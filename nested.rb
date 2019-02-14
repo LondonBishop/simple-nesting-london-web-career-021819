528f19c4b873831fcb271b08cf3cee4002e3e5f4
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-14 10:59:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,7 +135,7 @@ def adding_to_dennis
         }
      }
      
-     progArray = programmer_hash[:dennis_ritchie][:languages][].push("Assembley")
+     progArray = programmer_hash[:dennis_ritchie][:languages][-1] = "Assembley"
      return programmer_hash
     
 end
