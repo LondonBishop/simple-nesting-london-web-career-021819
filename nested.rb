0d6d1c21a2b71016553376f5b00bdb193701ef98
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-14 11:11:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -139,8 +139,11 @@ def adding_to_dennis
      #programmer_hash[:dennis_ritchie][:languages].push("Assembley")
      
     progArray = programmer_hash[:dennis_ritchie][:languages]
+    binding.pry
     progArray.push("Assembley")
+    binding.pry
     programmer_hash[:dennis_ritchie][:languages] = progArray
+    binding.pry
     return programmer_hash
     
 end
